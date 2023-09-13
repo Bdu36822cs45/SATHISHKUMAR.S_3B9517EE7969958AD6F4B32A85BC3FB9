0c5fe2e4f4b3388622548e5d3549b04c578c32e7
Bdu36822cs45 <144877449+Bdu36822cs45@users.noreply.github.com>
2023-09-13 11:21:18 +0530

    Add files via upload

---
+++ main (7).py
@@ -0,0 +1,9 @@
+a=int(input("enter a number:"))
+if a==1:
+  print("a is equal to 1")
+elif(a%2)==0:
+  print("a is an even number")
+elif a<0:
+  print("a is a negative number")
+else:
+  print("a is a positive odd number other than 1")
